ce0badaf5a5e6103adb1de0cb66ea5daec4c9774
Jan Smid <jsmid@redhat.com>
2025-11-18 13:34:50 +0100

    [RHTAP-5669] Implements checks for Artifactory plugin (#221)

--- src/ui/plugins/registry/artifactoryUiPlugin.ts
+++ src/ui/plugins/registry/artifactoryUiPlugin.ts
@@ -3,27 +3,26 @@ import { ImageRegistry } from '../../../rhtap/core/integration/registry/imageReg
 import { BaseRegistryPlugin } from "./baseRegistryPlugin";
 import { RegistryPO } from "../../page-objects/registryPo";
 
-export class NexusUiPlugin extends BaseRegistryPlugin {
+export class ArtifactoryUiPlugin extends BaseRegistryPlugin {
 
     constructor(registry: ImageRegistry) {
         super(registry);
     }
 
     async checkRepositoryHeading(page: Page): Promise<void> {
-        await expect(page.getByRole('heading', { name: `${RegistryPO.nexusRepositoryPrefix} ${this.registry.getOrganization()}/${this.registry.getImageName()}` })).toBeVisible();
+        await expect(page.getByRole('heading', { name: `${RegistryPO.artifactoryRepositoryPrefix} ${this.registry.getImageName()}` })).toBeVisible();
     }
 
     // eslint-disable-next-line no-unused-vars
     async checkRepositoryLink(_page: Page): Promise<void> {
-        // Skipped: Nexus repository link is not supported yet
+        // Skipped: Artifactory repository link is not supported yet
     }
 
     async checkTableColumns(page: Page): Promise<void> {
         await this.checkTableColumnHeaders(page, [
             RegistryPO.versionColumnHeader,
-            RegistryPO.artifactColumnHeader,
-            RegistryPO.repositoryTypeColumnHeader,
-            RegistryPO.checksumColumnHeader,
+            RegistryPO.repositoriesColumnHeader,
+            RegistryPO.manifestColumnHeader,
             RegistryPO.modifiedColumnHeader,
             RegistryPO.sizeColumnHeader,
         ]);
