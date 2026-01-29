506314bbdd6f4d9d86179631697042671459003f
Jan Smid <124383262+jsmid1@users.noreply.github.com>
2026-01-29 11:02:02 +0100

    [disable-githubactions-ui-check] Disables Githubactions check in UI tests (#281)

--- tests/ui/component.test.ts
+++ tests/ui/component.test.ts
@@ -5,6 +5,7 @@ import { CommonPO } from '../../src/ui/page-objects/commonPo';
 import { hideQuickStartIfVisible } from '../../src/ui/commonUi';
 import { waitForPageLoad } from '../../src/ui/commonUi';
 import { AUTH_STORAGE_FILE } from '../../playwright.config';
+import { GithubActionsPlugin } from '../../src/ui/plugins/ci/githubActionsPlugin';
 
 /**
  * Create a basic test fixture with testItem
@@ -77,7 +78,8 @@ test.describe('Component UI Test Suite', () => {
     test('verify CI provider on CI tab', async ({ page }) => {
       const ciPlugin = component.getCI();
 
-      if (ciPlugin === undefined) {
+      // Skip test for Github Actions plugin until issue is resolved: https://issues.redhat.com/browse/RHTAP-6138
+      if (ciPlugin === undefined || ciPlugin instanceof GithubActionsPlugin) {
         console.warn(`Skipping CI test as testing ${component.getCoreComponent().getCI().getCIType()} is not supported`);
         test.skip();
         return;
