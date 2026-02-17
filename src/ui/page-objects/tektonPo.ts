bb946219f1fd44f56177a9c50ca9efc95566c3af
BohdanMar <bmarcynk@redhat.com>
2026-02-17 10:39:29 +0100

    [RHTAP-6188] Update Tekton task names to match docker-build-tssc pipeline
    Update sourceTasks and gitopsTasks in tektonPo.ts to match the new
    docker-build-tssc and gitops-pull-request-tssc pipeline definitions.

--- src/ui/page-objects/tektonPo.ts
+++ src/ui/page-objects/tektonPo.ts
@@ -26,6 +26,6 @@ export const TektonPO = {
 	durationRegex: /\d+\s+minutes?\s+\d+\s+seconds?/,
 
 	// Task names
-    sourceTasks: ['init', 'clone-repository', 'build-container', 'update-deployment', 'acs-image-scan', 'acs-image-check', 'acs-deploy-check', 'show-summary', 'show-sbom'],
-	gitopsTasks: ['clone-repository', 'get-images-to-verify', 'get-images-to-upload-sbom', 'download-sboms', 'upload-sboms-to-trustification', 'verify-enterprise-contract'],
+    sourceTasks: ['clone-repository', 'build', 'deploy', 'deployment-check', 'scan', 'show-sbom', 'summarize'],
+	gitopsTasks: ['clone-repository', 'get-images', 'verify-conforma', 'download-sboms', 'upload-sboms'],
 };
